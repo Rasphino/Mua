e9b8548fbb09a7e20ba6122eec1796cd00dc92ee
Honghao Li <hh.li99@outlook.com>
2019-10-19 00:10:30 +0800

    deal with comment

--- src/mua/Main.java
+++ src/mua/Main.java
@@ -7,14 +7,24 @@ public class Main {
     private static HashMap<String, String> variables = new HashMap<>();
 
     public static void main(String[] args) {
-        System.out.print(">>> ");
         String cmd_in;
+        int idx;
 
-        while (in.hasNext() && !(cmd_in = in.nextLine()).equals("exit")) {
-            // TODO
-            System.out.println("Unknown input '" + cmd_in + "'");
-            ArrayList<String> tokens = src.mua.Lexer.parse(cmd_in);
+        System.out.print(">>> ");
+
+        while (in.hasNextLine() && !(cmd_in = in.nextLine()).equals("exit")) {
+            List<String> tokens = src.mua.Lexer.parse(cmd_in);
             System.out.println(tokens);
+
+            // 处理注释（要是java有goto就好了
+            if ((idx = tokens.indexOf("//")) != -1) tokens.subList(idx, tokens.size()).clear();
+            if (tokens.isEmpty() || tokens.get(0).equals("//")) {
+                System.out.print(">>> ");
+                continue;
+            }
+            System.out.println(tokens);
+
+            // TODO
 
             System.out.print(">>> ");
         }
